031325c6adc647b1d0c44751930f4733da7eca01
Howardchann <stallonh@126.com>
2026-09-26 15:28:39 +0800

    stamp bb6256f (v1.1.22)

--- miniprogram/utils/build-info.js
+++ miniprogram/utils/build-info.js
@@ -2,8 +2,8 @@
 // 在开发者工具点「上传」或「预览」之前跑一次：node tools/stamp-build.js
 // 作用：让「设置 → 关于」显示手机上跑的到底是哪一次提交。
 module.exports = {
-  sha: 'a74a90c',
+  sha: 'bb6256f',
   branch: 'cloud-sync',
-  dirty: false,
-  builtAt: 1790406884926,
+  dirty: true,
+  builtAt: 1790407719606,
 };
