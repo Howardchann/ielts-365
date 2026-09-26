0d5d2d48c62663bbb0e253e99e98bc3032883fdb
Howardchann <stallonh@126.com>
2026-09-26 15:39:10 +0800

    stamp b31ea3a (v1.1.23)

--- miniprogram/utils/build-info.js
+++ miniprogram/utils/build-info.js
@@ -2,8 +2,8 @@
 // 在开发者工具点「上传」或「预览」之前跑一次：node tools/stamp-build.js
 // 作用：让「设置 → 关于」显示手机上跑的到底是哪一次提交。
 module.exports = {
-  sha: 'bb6256f',
+  sha: 'b31ea3a',
   branch: 'cloud-sync',
-  dirty: true,
-  builtAt: 1790407719606,
+  dirty: false,
+  builtAt: 1790408349607,
 };
