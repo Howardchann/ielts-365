71d7da90e420b67363809bdb8e0f280b28fe3e08
Howardchann <stallonh@126.com>
2026-09-26 12:16:15 +0800

    chore(stamp): v1.1.14 build stamp

--- miniprogram/utils/build-info.js
+++ miniprogram/utils/build-info.js
@@ -2,8 +2,8 @@
 // 在开发者工具点「上传」或「预览」之前跑一次：node tools/stamp-build.js
 // 作用：让「设置 → 关于」显示手机上跑的到底是哪一次提交。
 module.exports = {
-  sha: '3044d0f',
+  sha: '984993c',
   branch: 'cloud-sync',
   dirty: false,
-  builtAt: 1790395378714,
+  builtAt: 1790396174798,
 };
