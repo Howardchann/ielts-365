f16653f5e9eaa78aba2cc3b726a5505cd25f94f5
Howardchann <stallonh@126.com>
2026-09-26 15:14:55 +0800

    stamp a74a90c (v1.1.21)

--- miniprogram/utils/build-info.js
+++ miniprogram/utils/build-info.js
@@ -2,8 +2,8 @@
 // 在开发者工具点「上传」或「预览」之前跑一次：node tools/stamp-build.js
 // 作用：让「设置 → 关于」显示手机上跑的到底是哪一次提交。
 module.exports = {
-  sha: '8d66616',
+  sha: 'a74a90c',
   branch: 'cloud-sync',
   dirty: false,
-  builtAt: 1790405806179,
+  builtAt: 1790406884926,
 };
