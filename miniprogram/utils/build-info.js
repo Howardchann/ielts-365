e894bfe421463761037d705459559d60db353faf
Howardchann <stallonh@126.com>
2026-09-26 17:52:03 +0800

    stamp v1.1.32

--- miniprogram/utils/build-info.js
+++ miniprogram/utils/build-info.js
@@ -2,8 +2,8 @@
 // 在开发者工具点「上传」或「预览」之前跑一次：node tools/stamp-build.js
 // 作用：让「设置 → 关于」显示手机上跑的到底是哪一次提交。
 module.exports = {
-  sha: '54311f3',
+  sha: '37d3947',
   branch: 'cloud-sync',
   dirty: false,
-  builtAt: 1790415500443,
+  builtAt: 1790416323101,
 };
